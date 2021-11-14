3bfee94d3f6c96813c0450d8ab361ef79f31dd24
shermerL <xizhizeze@126.com>
2021-11-14 16:24:28 +0800

    Site updated: 2021-11-14 16:24:27

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2021/9/01 00:00:00");
+  var start = new Date("2021/10/06 00:00:00");
 
   function update() {
     var now = new Date();
